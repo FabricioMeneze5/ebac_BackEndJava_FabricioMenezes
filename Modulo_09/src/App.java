987b9c5342de0cb3712be3acafb8cc4f92da603a
FabricioMeneze5 <fabricio-menezes@live.com>
2025-02-22 22:04:52 +0000

    upload Modulo_09

---
+++ Modulo_09/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numPrimitivo = 42;
+        Integer numWrapper = numPrimitivo; 
+
+        System.out.println("Primitivo: " + numPrimitivo);
+        System.out.println("Wrapper: " + numWrapper);
+    }
+}
